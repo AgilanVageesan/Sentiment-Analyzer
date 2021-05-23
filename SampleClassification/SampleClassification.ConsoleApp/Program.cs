941df5f3d460f47fad5306c2cc27873248fd9a01
Agilan Vageesan <agilanvageesan@gmail.com>
2021-05-23 23:18:05 +0530

    Sentimenti

---
+++ SampleClassification/SampleClassification.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using SampleClassification.Model;
+
+namespace SampleClassification.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool analyze = true;
+            while (analyze)
+            {
+                string val;
+                Console.Write("Enter Text: ");
+                val = Console.ReadLine();
+                var input = new ModelInput()
+                {
+                    Col0 = val
+                };
+
+                ModelOutput result = ConsumeModel.Predict(input);
+                string sentiment = result.Prediction == "1" ? "Positive" : "Negative";
+                Console.WriteLine($"Text: {input.Col0}\nSentiment: {sentiment}");
+                Console.WriteLine("----------------------------------------");
+            }
+        }
+    }
+}
